4a45de603e20246ec9831de9e286d916f1f6b53e
Gowtham-1999 <42689909+Gowtham-1999@users.noreply.github.com>
2020-07-16 12:19:46 +0530

    Assignment2

--- ASSIGNMENT2/ass2_2.js
+++ ASSIGNMENT2/ass2_2.js
@@ -1,10 +1,39 @@
-// Question 3 : Ask the user if he is 21+ and the value "Can drink" , " Cannot drink"
+// Question 2 : Explain with example  remaing methods of String and Array
 
-let age = confirm("Are your 21+ if yes click ok");
-if(age){
-    document.write("Can drink");
-}
+// SEARCH 
 
-else{
-    document.write("Cannot drink");
-}
+let str = "We can lear java script as fast as its easy to understand    ";
+
+console.log(str.search("java"));
+
+// SLICE
+
+console.log(str.slice(7,23));
+
+// SUBSTRING
+
+console.log(str.substring(23,31));
+
+console.log(str.substr(32));
+
+// Replacing
+
+console.log(str.replace("as fast as","as quickly because" ));
+
+//trim
+
+console.log(str.trim());
+
+var fruits = ['banana','orange']
+
+// join
+console.log(fruits.join('*'));
+
+// tostring
+console.log(fruits.toString());
+
+// changing the element
+fruits[0] = 'mango';
+console.log(fruits)
+
+
