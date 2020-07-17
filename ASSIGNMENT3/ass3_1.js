bfca16b9b5ee786088e421225723cc2e90f34639
Gowtham-1999 <42689909+Gowtham-1999@users.noreply.github.com>
2020-07-17 12:14:25 +0530

    Create ass3_1.js

---
+++ ASSIGNMENT3/ass3_1.js
@@ -0,0 +1,18 @@
+//Question 1:
+let num = +prompt("Enter a Number", '0');
+
+function check(numbers) {
+    let checker ;
+    if (num % 2 == 0){
+        checker = 'even';
+        return checker;
+    }
+    else{
+        checker = 'odd';
+        return checker;
+    }
+    
+}
+let variable = check(num);
+
+console.log(`The Number entered is ${num} and Number is ${variable}`);
