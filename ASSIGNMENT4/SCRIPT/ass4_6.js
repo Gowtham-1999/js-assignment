a6cb77f36d3d8770e46e3adfef0803c60eb645d6
Gowtham-1999 <42689909+Gowtham-1999@users.noreply.github.com>
2020-07-20 13:18:47 +0530

    Assignment4

---
+++ ASSIGNMENT4/SCRIPT/ass4_6.js
@@ -0,0 +1,11 @@
+let num = 0
+while(num<=100 ) {
+    num = prompt("Enter the number greater than hundred");
+    if(num == null){
+        num = (num == null ) ? "They cancel":"its not cancelled";
+        console.log(num)
+}
+else if(num>100){
+console.log(num)
+}
+}
